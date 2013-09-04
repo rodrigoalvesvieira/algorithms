713ee488fd757ce0c1aa847e90c7320224ab2e31
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-09-04 10:49:54 -0300

    Update Test.java

--- src/avl/Test.java
+++ src/avl/Test.java
@@ -4,5 +4,12 @@ package avl;
 public class Test {
 	public static void main(String[] args) {
 		AVL tree = new AVL();
+		
+		AVL.insert(tree, 1, 20, 0, 0, 10);
+		AVL.insert(tree, 1, 12, 0, 0, 10);
+		AVL.insert(tree, 1, 21, 0, 0, 10);
+		
+		AVL.inOrder(tree);
+		
 	}
 }
